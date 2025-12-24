51cac2dba8501314027db5daa532a4fd7202995d
adefio <adefiopratama7@gmail.com>
2025-12-24 15:49:58 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,15 +2,24 @@ require('dotenv').config();
 const createServer = require('./Infrastructures/http/createServer');
 const container = require('./Infrastructures/container');
 
-const start = async () => {
+const initServer = async () => {
   const server = await createServer(container);
-  
+
   if (process.env.NODE_ENV !== 'production') {
     await server.start();
-    console.log(`server start at ${server.info.uri}`);
+    console.log(`Server berjalan lokal pada ${server.info.uri}`);
+  } else {
+    await server.initialize();
   }
 
   return server;
 };
 
-module.exports = start();
+module.exports = async (req, res) => {
+  const server = await initServer();
+  server.listener(req, res);
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  initServer();
+}
