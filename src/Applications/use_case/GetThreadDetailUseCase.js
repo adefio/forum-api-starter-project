64e919dbbea8e53bbda259724b74178e2c98ddc1
adefio <adefiopratama7@gmail.com>
2025-12-24 21:30:13 +0700

    Update GetThreadDetailUseCase.js

--- src/Applications/use_case/GetThreadDetailUseCase.js
+++ src/Applications/use_case/GetThreadDetailUseCase.js
@@ -1,105 +1,36 @@
-const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
-const CommentRepository = require('../../../Domains/comments/CommentRepository');
-const ReplyRepository = require('../../../Domains/replies/ReplyRepository');
-const GetThreadDetailUseCase = require('../GetThreadDetailUseCase');
+class GetThreadDetailUseCase {
+  constructor({ threadRepository, commentRepository, replyRepository }) {
+    this._threadRepository = threadRepository;
+    this._commentRepository = commentRepository;
+    this._replyRepository = replyRepository;
+  }
 
-describe('GetThreadDetailUseCase', () => {
-  it('should orchestrating the get thread detail action correctly', async () => {
-    // Arrange
-    const threadId = 'thread-123';
-    const mockThreadDetail = {
-      id: threadId,
-      title: 'sebuah thread',
-      body: 'sebuah body thread',
-      date: '2021-08-08T07:19:09.775Z',
-      username: 'dicoding',
-    };
+  async execute(threadId) {
+    await this._threadRepository.verifyThreadAvailability(threadId);
+    const thread = await this._threadRepository.getThreadById(threadId);
+    const comments = await this._commentRepository.getCommentsByThreadId(threadId);
+    const replies = await this._replyRepository.getRepliesByThreadId(threadId);
 
-    const mockComments = [
-      {
-        id: 'comment-1',
-        username: 'johndoe',
-        date: '2021-08-08T07:22:33.555Z',
-        content: 'sebuah comment',
-        is_delete: false,
-        like_count: 2, 
-      },
-      {
-        id: 'comment-2',
-        username: 'dicoding',
-        date: '2021-08-08T07:26:21.338Z',
-        content: 'sebuah comment',
-        is_delete: true,
-        like_count: 0, 
-      },
-    ];
-
-    const mockReplies = [
-      {
-        id: 'reply-1',
-        content: 'sebuah balasan',
-        date: '2021-08-08T07:28:33.555Z',
-        username: 'johndoe',
-        comment_id: 'comment-1',
-        is_delete: false,
-      },
-    ];
-
-    /** creating dependency of use case */
-    const mockThreadRepository = new ThreadRepository();
-    const mockCommentRepository = new CommentRepository();
-    const mockReplyRepository = new ReplyRepository();
-
-    /** mocking needed function */
-    mockThreadRepository.verifyThreadAvailability = jest.fn(() => Promise.resolve());
-    mockThreadRepository.getThreadById = jest.fn(() => Promise.resolve(mockThreadDetail));
-    mockCommentRepository.getCommentsByThreadId = jest.fn(() => Promise.resolve(mockComments));
-    mockReplyRepository.getRepliesByThreadId = jest.fn(() => Promise.resolve(mockReplies));
-
-    /** creating use case instance */
-    const getThreadDetailUseCase = new GetThreadDetailUseCase({
-      threadRepository: mockThreadRepository,
-      commentRepository: mockCommentRepository,
-      replyRepository: mockReplyRepository,
+    thread.comments = comments.map((comment) => {
+      const commentReplies = replies.filter((reply) => reply.comment_id === comment.id);
+      
+      return {
+        id: comment.id,
+        username: comment.username,
+        date: comment.date,
+        replies: commentReplies.map((reply) => ({
+          id: reply.id,
+          content: reply.is_delete ? '**balasan telah dihapus**' : reply.content,
+          date: reply.date,
+          username: reply.username,
+        })),
+        content: comment.is_delete ? '**komentar telah dihapus**' : comment.content,
+        likeCount: comment.like_count, 
+      };
     });
 
-    const expectedThread = {
-      ...mockThreadDetail,
-      comments: [
-        {
-          id: 'comment-1',
-          username: 'johndoe',
-          date: '2021-08-08T07:22:33.555Z',
-          content: 'sebuah comment',
-          likeCount: 2, 
-          replies: [
-            {
-              id: 'reply-1',
-              content: 'sebuah balasan',
-              date: '2021-08-08T07:28:33.555Z',
-              username: 'johndoe',
-            },
-          ],
-        },
-        {
-          id: 'comment-2',
-          username: 'dicoding',
-          date: '2021-08-08T07:26:21.338Z',
-          content: '**komentar telah dihapus**',
-          likeCount: 0, 
-          replies: [],
-        },
-      ],
-    };
+    return thread;
+  }
+}
 
-    // Action
-    const thread = await getThreadDetailUseCase.execute(threadId);
-
-    // Assert
-    expect(thread).toStrictEqual(expectedThread);
-    expect(mockThreadRepository.verifyThreadAvailability).toBeCalledWith(threadId);
-    expect(mockThreadRepository.getThreadById).toBeCalledWith(threadId);
-    expect(mockCommentRepository.getCommentsByThreadId).toBeCalledWith(threadId);
-    expect(mockReplyRepository.getRepliesByThreadId).toBeCalledWith(threadId);
-  });
-});
+module.exports = GetThreadDetailUseCase;
