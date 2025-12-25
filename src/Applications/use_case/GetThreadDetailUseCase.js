fe1e978a0bb62f809a356102d410fb06af2c4ac8
adefio <adefiopratama7@gmail.com>
2025-12-25 10:05:40 +0700

    p

--- src/Applications/use_case/GetThreadDetailUseCase.js
+++ src/Applications/use_case/GetThreadDetailUseCase.js
@@ -1,3 +1,5 @@
+const GetThreadDetailUseCase = require('./GetThreadDetailUseCase');
+
 class GetThreadDetailUseCase {
   constructor({ threadRepository, commentRepository, replyRepository }) {
     this._threadRepository = threadRepository;
@@ -6,30 +8,42 @@ class GetThreadDetailUseCase {
   }
 
   async execute(threadId) {
+    // 1. Validasi keberadaan thread
     await this._threadRepository.verifyThreadAvailability(threadId);
-    const thread = await this._threadRepository.getThreadById(threadId);
-    const comments = await this._commentRepository.getCommentsByThreadId(threadId);
-    const replies = await this._replyRepository.getRepliesByThreadId(threadId);
 
-    thread.comments = comments.map((comment) => {
-      const commentReplies = replies.filter((reply) => reply.comment_id === comment.id);
-      
-      return {
-        id: comment.id,
-        username: comment.username,
-        date: comment.date,
-        replies: commentReplies.map((reply) => ({
+    // 2. Ambil semua data terkait secara paralel untuk performa lebih baik
+    const [thread, comments, replies] = await Promise.all([
+      this._threadRepository.getThreadById(threadId),
+      this._commentRepository.getCommentsByThreadId(threadId),
+      this._replyRepository.getRepliesByThreadId(threadId),
+    ]);
+
+    // 3. Mapping komentar dan masukkan balasan ke dalamnya
+    const mappedComments = comments.map((comment) => {
+      const commentReplies = replies
+        .filter((reply) => reply.comment_id === comment.id)
+        .map((reply) => ({
           id: reply.id,
           content: reply.is_delete ? '**balasan telah dihapus**' : reply.content,
           date: reply.date,
           username: reply.username,
-        })),
+        }));
+
+      return {
+        id: comment.id,
+        username: comment.username,
+        date: comment.date,
         content: comment.is_delete ? '**komentar telah dihapus**' : comment.content,
-        likeCount: comment.like_count, 
+        likeCount: comment.like_count || 0, // Mengubah snake_case ke camelCase
+        replies: commentReplies,
       };
     });
 
-    return thread;
+    // 4. Kembalikan objek thread utuh dengan komentar yang sudah ter-mapping
+    return {
+      ...thread,
+      comments: mappedComments,
+    };
   }
 }
 
--- src/Applications/use_case/_test/GetThreadDetailUseCase.test.js
+++ src/Applications/use_case/_test/GetThreadDetailUseCase.test.js
@@ -22,15 +22,7 @@ describe('GetThreadDetailUseCase', () => {
         date: '2021-08-08T07:22:33.555Z',
         content: 'sebuah comment',
         is_delete: false,
-        like_count: 0, 
-      },
-      {
-        id: 'comment-2',
-        username: 'dicoding',
-        date: '2021-08-08T07:26:21.338Z',
-        content: 'sebuah comment',
-        is_delete: true,
-        like_count: 0, 
+        like_count: 2,
       },
     ];
 
@@ -43,43 +35,41 @@ describe('GetThreadDetailUseCase', () => {
         comment_id: 'comment-1',
         is_delete: false,
       },
-      {
-        id: 'reply-2',
-        content: 'sebuah balasan',
-        date: '2021-08-08T07:30:33.555Z',
-        username: 'dicoding',
-        comment_id: 'comment-1',
-        is_delete: true,
-      },
     ];
 
-    /** creating dependency of use case */
+    /** Creating dependency and mocking */
     const mockThreadRepository = new ThreadRepository();
     const mockCommentRepository = new CommentRepository();
     const mockReplyRepository = new ReplyRepository();
 
-    /** mocking needed function */
     mockThreadRepository.verifyThreadAvailability = jest.fn(() => Promise.resolve());
     mockThreadRepository.getThreadById = jest.fn(() => Promise.resolve(mockThreadDetail));
     mockCommentRepository.getCommentsByThreadId = jest.fn(() => Promise.resolve(mockComments));
     mockReplyRepository.getRepliesByThreadId = jest.fn(() => Promise.resolve(mockReplies));
 
-    /** creating use case instance */
     const getThreadDetailUseCase = new GetThreadDetailUseCase({
       threadRepository: mockThreadRepository,
       commentRepository: mockCommentRepository,
       replyRepository: mockReplyRepository,
     });
 
+    // Action
+    const threadDetail = await getThreadDetailUseCase.execute(threadId);
+
+    // Assert
     const expectedThread = {
-      ...mockThreadDetail,
+      id: 'thread-123',
+      title: 'sebuah thread',
+      body: 'sebuah body thread',
+      date: '2021-08-08T07:19:09.775Z',
+      username: 'dicoding',
       comments: [
         {
           id: 'comment-1',
           username: 'johndoe',
           date: '2021-08-08T07:22:33.555Z',
           content: 'sebuah comment',
-          likeCount: 0, 
+          likeCount: 2,
           replies: [
             {
               id: 'reply-1',
@@ -87,30 +77,12 @@ describe('GetThreadDetailUseCase', () => {
               date: '2021-08-08T07:28:33.555Z',
               username: 'johndoe',
             },
-            {
-              id: 'reply-2',
-              content: '**balasan telah dihapus**',
-              date: '2021-08-08T07:30:33.555Z',
-              username: 'dicoding',
-            },
           ],
-        },
-        {
-          id: 'comment-2',
-          username: 'dicoding',
-          date: '2021-08-08T07:26:21.338Z',
-          content: '**komentar telah dihapus**',
-          likeCount: 0, 
-          replies: [],
         },
       ],
     };
 
-    // Action
-    const thread = await getThreadDetailUseCase.execute(threadId);
-
-    // Assert
-    expect(thread).toStrictEqual(expectedThread);
+    expect(threadDetail).toStrictEqual(expectedThread);
     expect(mockThreadRepository.verifyThreadAvailability).toBeCalledWith(threadId);
     expect(mockThreadRepository.getThreadById).toBeCalledWith(threadId);
     expect(mockCommentRepository.getCommentsByThreadId).toBeCalledWith(threadId);
