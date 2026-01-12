3f59ca38e81ba5ebf8a0b244ad5492aef7643e2a
adefio <adefiopratama7@gmail.com>
2026-01-12 15:56:29 +0700

    Update routes.js

--- src/Interfaces/http/api/users/routes.js
+++ src/Interfaces/http/api/users/routes.js
@@ -1,17 +1,20 @@
-const routes = (handler) => ([
-  {
-    method: 'POST',
-    path: '/users',
-    handler: handler.postUserHandler,
-  },
-  {
-    method: 'GET',
-    path: '/users',
-    handler: () => ({
+const express = require('express');
+
+const routes = (handler) => {
+  const router = express.Router();
+
+  // POST /users (Rute umum - Pendaftaran User baru tidak butuh authMiddleware)
+  router.post('/', (req, res, next) => handler.postUserHandler(req, res, next));
+
+  // GET /users (Cek status layanan)
+  router.get('/', (req, res) => {
+    res.json({
       status: 'success',
       message: 'User service is ready',
-    }),
-  },
-]);
+    });
+  });
+
+  return router;
+};
 
 module.exports = routes;
