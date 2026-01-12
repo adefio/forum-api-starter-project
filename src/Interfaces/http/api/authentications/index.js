7530a2a6713422abc3d7d6cb71ac19af7b987fc2
adefio <adefiopratama7@gmail.com>
2026-01-12 10:58:29 +0700

    Update index.js

--- src/Interfaces/http/api/authentications/index.js
+++ src/Interfaces/http/api/authentications/index.js
@@ -1,10 +1,7 @@
-const routes = require('./routes');
 const AuthenticationsHandler = require('./handler');
+const routes = require('./routes');
 
-module.exports = {
-  name: 'authentications',
-  register: async (server, { container }) => {
-    const authenticationsHandler = new AuthenticationsHandler(container);
-    server.route(routes(authenticationsHandler));
-  },
+module.exports = (container) => {
+  const authenticationsHandler = new AuthenticationsHandler(container);
+  return routes(authenticationsHandler);
 };
