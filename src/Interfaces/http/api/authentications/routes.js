f8cc77be18b93eae30f8fe08d08b5a915231aca4
adefio <adefiopratama7@gmail.com>
2026-01-12 15:39:28 +0700

    Update routes.js

--- src/Interfaces/http/api/authentications/routes.js
+++ src/Interfaces/http/api/authentications/routes.js
@@ -1,27 +1,26 @@
-const routes = (handler) => ([
-  {
-    method: 'POST',
-    path: '/authentications',
-    handler: handler.postAuthenticationHandler,
-  },
-  {
-    method: 'PUT',
-    path: '/authentications',
-    handler: handler.putAuthenticationHandler,
-  },
-  {
-    method: 'DELETE',
-    path: '/authentications',
-    handler: handler.deleteAuthenticationHandler,
-  },
-  {
-    method: 'GET',
-    path: '/authentications',
-    handler: () => ({
+const express = require('express');
+
+const routes = (handler) => {
+  const router = express.Router();
+
+  // POST /authentications (Login)
+  router.post('/', handler.postAuthenticationHandler);
+
+  // PUT /authentications (Refresh Token)
+  router.put('/', handler.putAuthenticationHandler);
+
+  // DELETE /authentications (Logout)
+  router.delete('/', handler.deleteAuthenticationHandler);
+
+  // GET /authentications (Check Status)
+  router.get('/', (req, res) => {
+    res.json({
       status: 'success',
       message: 'Authentication service is ready',
-    }),
-  },
-]);
+    });
+  });
+
+  return router;
+};
 
 module.exports = routes;
