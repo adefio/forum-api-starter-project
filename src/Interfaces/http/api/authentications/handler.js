05c36ef7c7f248b7e61af1e0b95ff67d956c7ead
adefio <adefiopratama7@gmail.com>
2026-01-12 11:03:08 +0700

    Update handler.js

--- src/Interfaces/http/api/authentications/handler.js
+++ src/Interfaces/http/api/authentications/handler.js
@@ -11,39 +11,51 @@ class AuthenticationsHandler {
     this.deleteAuthenticationHandler = this.deleteAuthenticationHandler.bind(this);
   }
 
-  async postAuthenticationHandler(request, h) {
-    const loginUserUseCase = this._container.getInstance(LoginUserUseCase.name);
-    const { accessToken, refreshToken } = await loginUserUseCase.execute(request.payload);
-    const response = h.response({
-      status: 'success',
-      data: {
-        accessToken,
-        refreshToken,
-      },
-    });
-    response.code(201);
-    return response;
+  async postAuthenticationHandler(req, res, next) {
+    try {
+      const loginUserUseCase = this._container.getInstance(LoginUserUseCase.name);
+      const { accessToken, refreshToken } = await loginUserUseCase.execute(req.body);
+      
+      res.status(201).json({
+        status: 'success',
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  async putAuthenticationHandler(request) {
-    const refreshAuthenticationUseCase = this._container
-      .getInstance(RefreshAuthenticationUseCase.name);
-    const accessToken = await refreshAuthenticationUseCase.execute(request.payload);
+  async putAuthenticationHandler(req, res, next) {
+    try {
+      const refreshAuthenticationUseCase = this._container
+        .getInstance(RefreshAuthenticationUseCase.name);
+      const accessToken = await refreshAuthenticationUseCase.execute(req.body);
 
-    return {
-      status: 'success',
-      data: {
-        accessToken,
-      },
-    };
+      res.status(200).json({
+        status: 'success',
+        data: {
+          accessToken,
+        },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  async deleteAuthenticationHandler(request) {
-    const logoutUserUseCase = this._container.getInstance(LogoutUserUseCase.name);
-    await logoutUserUseCase.execute(request.payload);
-    return {
-      status: 'success',
-    };
+  async deleteAuthenticationHandler(req, res, next) {
+    try {
+      const logoutUserUseCase = this._container.getInstance(LogoutUserUseCase.name);
+      await logoutUserUseCase.execute(req.body);
+      
+      res.status(200).json({
+        status: 'success',
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
