fa9b22ea413a801e94960b35ef7a72ee9c104e61
adefio <adefiopratama7@gmail.com>
2026-01-19 00:21:29 +0700

    Update index.js

--- src/Interfaces/http/api/replies/index.js
+++ src/Interfaces/http/api/replies/index.js
@@ -1,7 +1,23 @@
-const RepliesHandler = require('./handler');
+const express = require('express');
 const routes = require('./routes');
+const RepliesHandler = require('./handler');
 
-module.exports = (container) => {
+/**
+ * Fungsi factory untuk menginisialisasi router balasan (replies).
+ * @param {object} container - Dependency Injection Container.
+ * @returns {express.Router} - Router Express yang sudah terkonfigurasi.
+ */
+const replies = (container) => {
   const repliesHandler = new RepliesHandler(container);
-  return routes(repliesHandler);
+  
+  /**
+   * PERBAIKAN: Mengaktifkan mergeParams: true.
+   * Sangat penting agar router ini dapat membaca parameter :threadId dan :commentId 
+   * yang didefinisikan pada router induk (createServer.js).
+   */
+  const router = express.Router({ mergeParams: true });
+  
+  return routes(router, repliesHandler);
 };
+
+module.exports = replies;
