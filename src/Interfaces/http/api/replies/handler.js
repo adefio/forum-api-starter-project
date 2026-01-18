a7b41f65ff34f5e52fd32c41fb6778e8db2f6c88
adefio <adefiopratama7@gmail.com>
2026-01-19 06:15:06 +0700

    Update handler.js

--- src/Interfaces/http/api/replies/handler.js
+++ src/Interfaces/http/api/replies/handler.js
@@ -1,3 +1,4 @@
+// src/Interfaces/http/api/replies/handler.js
 const autoBind = require('auto-bind');
 const AddReplyUseCase = require('../../../../Applications/use_case/AddReplyUseCase');
 const DeleteReplyUseCase = require('../../../../Applications/use_case/DeleteReplyUseCase');
@@ -5,37 +6,38 @@ const DeleteReplyUseCase = require('../../../../Applications/use_case/DeleteRepl
 class RepliesHandler {
   constructor(container) {
     this._container = container;
-
     autoBind(this);
   }
 
-  async postReplyHandler(request, h) {
-    const addReplyUseCase = this._container.getInstance(AddReplyUseCase.name);
-    const { threadId, commentId } = request.params;
-    const { id: credentialId } = request.auth.credentials;
+  async postReplyHandler(req, res, next) {
+    try {
+      const addReplyUseCase = this._container.getInstance(AddReplyUseCase.name);
+      const { threadId, commentId } = req.params;
+      const { id: credentialId } = req.auth;
 
-    const addedReply = await addReplyUseCase.execute(request.payload, threadId, commentId, credentialId);
+      const addedReply = await addReplyUseCase.execute(req.body, threadId, commentId, credentialId);
 
-    const response = h.response({
-      status: 'success',
-      data: {
-        addedReply,
-      },
-    });
-    response.code(201);
-    return response;
+      res.status(201).json({
+        status: 'success',
+        data: { addedReply },
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
-  async deleteReplyHandler(request, h) {
-    const deleteReplyUseCase = this._container.getInstance(DeleteReplyUseCase.name);
-    const { threadId, commentId, replyId } = request.params;
-    const { id: credentialId } = request.auth.credentials;
+  async deleteReplyHandler(req, res, next) {
+    try {
+      const deleteReplyUseCase = this._container.getInstance(DeleteReplyUseCase.name);
+      const { threadId, commentId, replyId } = req.params;
+      const { id: credentialId } = req.auth;
 
-    await deleteReplyUseCase.execute(threadId, commentId, replyId, credentialId);
+      await deleteReplyUseCase.execute(threadId, commentId, replyId, credentialId);
 
-    return {
-      status: 'success',
-    };
+      res.status(200).json({ status: 'success' });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
