9d13364e918eeeb26b815a3a723b441a352a0657
adefio <adefiopratama7@gmail.com>
2026-01-19 08:15:33 +0700

    Update handler.js

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -1,6 +1,7 @@
 const autoBind = require('auto-bind');
 const AddCommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase');
 const DeleteCommentUseCase = require('../../../../Applications/use_case/DeleteCommentUseCase');
+const LikeCommentUseCase = require('../../../../Applications/use_case/LikeCommentUseCase'); 
 
 class CommentsHandler {
   constructor(container) {
@@ -12,15 +13,13 @@ class CommentsHandler {
     try {
       const addCommentUseCase = this._container.getInstance(AddCommentUseCase.name);
       const { threadId } = req.params;
-      const { id: credentialId } = req.auth; 
+      const { id: credentialId } = req.auth;
 
       const addedComment = await addCommentUseCase.execute(req.body, threadId, credentialId);
 
       res.status(201).json({
         status: 'success',
-        data: {
-          addedComment,
-        },
+        data: { addedComment },
       });
     } catch (error) {
       next(error);
@@ -35,9 +34,22 @@ class CommentsHandler {
 
       await deleteCommentUseCase.execute(threadId, commentId, credentialId);
 
-      res.status(200).json({
-        status: 'success',
-      });
+      res.status(200).json({ status: 'success' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  // 2. TAMBAHKAN METODE INI
+  async putLikeCommentHandler(req, res, next) {
+    try {
+      const likeCommentUseCase = this._container.getInstance(LikeCommentUseCase.name);
+      const { threadId, commentId } = req.params;
+      const { id: credentialId } = req.auth;
+
+      await likeCommentUseCase.execute(threadId, commentId, credentialId);
+
+      res.status(200).json({ status: 'success' });
     } catch (error) {
       next(error);
     }
